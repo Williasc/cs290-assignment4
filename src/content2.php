7218a37212f661867c8a83fd9fe8f9b6d06fb516
Scott Williams <williasc@onid.oregonstate.edu>
2015-05-03 00:18:59 +0000

    finished the rest of the assignment

--- src/content2.php
+++ src/content2.php
@@ -16,21 +16,14 @@ $filePath = implode('/',$filePath);
 $login = "http://" . $_SERVER['HTTP_HOST'] . $filePath . "/login.php";
 $content1 = "http://" . $_SERVER['HTTP_HOST'] . $filePath . "/content1.php";
 
-if($_POST['username'] == NULL){
+session_start();
+if(!isset($_SESSION['username'])){
     echo "You must login to view this page.<br>";
     echo "click <a href='" . $login . "'>here</a> to return to the login";
 }
 else{
-    session_start();
-}
-
-if(session_status() == PHP_SESSION_ACTIVE){
-    if(isset($_POST['username'])){
-        $_SESSION['username'] = $_POST['username'];
-    }
-    
-    echo "<h5<There's not much here.</h5>\n";
-    echo "Click <a = href='" . $login . "'>here</a> to return to the login.\n";
+    echo "<h5>There's not much here.</h5><br>";
+    echo "Click <a = href='" . $login . "'>here</a> to return to the login.<br>";
     echo "Click <a = href='" . $content1 . "'>here</a> to return to Content 1.\n";
 }
 
